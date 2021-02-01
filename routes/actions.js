3b3e07dc9ffca0e552ca14677499cbebabb46ea6
shubham1710 <shubhamstudent5@gmail.com>
2021-02-01 20:21:33 +0530

    Fixed the bugs

--- routes/actions.js
+++ routes/actions.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 const router = Router();
 const actionController = require('../controllers/actionControllers');
-const auth = require('../middleware/auth');
 
 router.post('like/:postId/:userId', actionController.like);
 router.post('comment/:postId/:userId', actionController.comment);
