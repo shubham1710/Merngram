dfb33ee90845d4bbf286342bb4106bb7e32adbad
shubham1710 <shubhamstudent5@gmail.com>
2021-02-01 19:15:21 +0530

    Added dependencies and server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const dotenv = require('dotenv');
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
+const dbURI = process.env.DBURI;
+const port = process.env.PORT || 4000;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));
