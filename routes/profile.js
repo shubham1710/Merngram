3fb5a0981e52649dccf8eb93282a6b0973b97541
shubham1710 <shubhamstudent5@gmail.com>
2021-02-02 08:46:40 +0530

    Added controller files

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const router = Router();
+const profileController = require('../controllers/profileControllers');
 
 router.get('/profile/:id', profileController.get_profile);
 router.get('/followers/:id', profileController.get_followers);
 router.get('/following/:id', profileController.get_following);
 profile.put('/profile/:id', profileController.update_profile);
+
+module.exports = router;
