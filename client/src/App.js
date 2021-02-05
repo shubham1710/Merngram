ff1e9567f4f12fc022b5fc28aac661739e9eb2d4
shubham1710 <shubhamstudent5@gmail.com>
2021-02-05 08:30:50 +0530

    Setup store and index reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
+import { Component } from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Merngram</h1>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Merngram</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
