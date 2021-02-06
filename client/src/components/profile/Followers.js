5296d3b8e2d1f6df4a206ddc18004e13b20545a2
shubham1710 <shubhamstudent5@gmail.com>
2021-02-06 14:06:56 +0530

    Added follow lists

--- client/src/components/profile/Followers.js
+++ client/src/components/profile/Followers.js
@@ -1,6 +1,36 @@
+import {Button} from 'reactstrap';
+
 const Followers = () => {
     return ( 
-        <div>Followers</div>
+        <div className="container mt-5">
+            <div className="height-100 row d-flex justify-content-center">
+                <div className="col-md-7">
+                <h3 className="mb-2">Followers</h3>
+                    <div className="p-3 bg-white rounded">
+                        <div className="d-flex justify-content-between">
+                            <div className="d-flex flex-row align-items-center">
+                                <div className="position-relative">
+                                    <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" width="80" className="rounded-circle"/>
+                                </div>
+                                <div className="ml-2">
+                                    <h5 className="mb-0">Kumar Shubham</h5>
+                                    <div className="d-flex flex-row">
+                                        <span className="mr-3">725 followers</span> 
+                                        <span>120 following</span>
+                                    </div>
+                                    <div class="mt-2">
+                                        <Button color="info" className="btn-sm">Follow</Button>
+                                        <Button color="danger" className="btn-sm">Unfollow</Button>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
  
