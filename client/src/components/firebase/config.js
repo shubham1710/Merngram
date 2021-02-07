d0fa4d1fa369f99537253f5f5b7b5440bc1bd95c
shubham1710 <shubhamstudent5@gmail.com>
2021-02-07 15:27:38 +0530

    Setup firebase storage

---
+++ client/src/components/firebase/config.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAM0Qx2Fa00mQDl8z1GVhM_ZABhgM-Gb8I",
+    authDomain: "merngram-2f709.firebaseapp.com",
+    projectId: "merngram-2f709",
+    storageBucket: "merngram-2f709.appspot.com",
+    messagingSenderId: "140589251373",
+    appId: "1:140589251373:web:ed8d3a897b03f4064471a4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+
+export {projectStorage};
