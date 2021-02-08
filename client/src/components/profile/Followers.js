65b754fa99103496fa7ccda11d38fa1b647ec858
shubham1710 <shubhamstudent5@gmail.com>
2021-02-08 20:52:06 +0530

    Follower and following completed

--- client/src/components/profile/Followers.js
+++ client/src/components/profile/Followers.js
@@ -2,9 +2,9 @@ import {Button} from 'reactstrap';
 import { getProfile } from '../../actions/profileActions';
 import { connect } from 'react-redux';
 import { useEffect, useState } from 'react';
-import {useParams} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 
-const Followers = ({profile, getProfile}) => {
+const Followers = ({profile, getProfile, user}) => {
     const { id } = useParams();
     const [Loading, setLoading] = useState(true);
 
@@ -18,28 +18,24 @@ const Followers = ({profile, getProfile}) => {
             <div className="height-100 row d-flex justify-content-center">
                 <div className="col-md-7">
                 <h3 className="mb-2">Followers</h3>
-                    {profile && profile.followers.map((follower) => (
-                        <div className="p-3 bg-white rounded">
+                {profile && profile.followers.map((follower) => (
+                    <div className="p-3 bg-white rounded">
                         <div className="d-flex justify-content-between">
                             <div className="d-flex flex-row align-items-center">
-                                <div className="position-relative">
-                                    <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" width="80" className="rounded-circle"/>
-                                </div>
+                                <Link to={`/profile/${follower.followerId}`}>
+                                    <div className="position-relative">
+                                        <img src={follower.followerPic} width="40" className="rounded-circle"/>
+                                    </div>
+                                </Link>
                                 <div className="ml-2">
-                                    <h5 className="mb-0">{follower.followerId}</h5>
-                                    <div className="d-flex flex-row">
-                                        <span className="mr-3">725 followers</span> 
-                                        <span>120 following</span>
-                                    </div>
-                                    <div class="mt-2">
-                                        <Button color="info" className="btn-sm">Follow</Button>
-                                        <Button color="danger" className="btn-sm">Unfollow</Button>
-                                    </div>
+                                    <Link to={`/profile/${follower.followerId}`}>
+                                        <h5 className="mb-0">{follower.followerName}</h5>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    ))}
+                ))}
                 </div>
             </div>
         </div>
@@ -47,7 +43,8 @@ const Followers = ({profile, getProfile}) => {
 }
 
 const mapStateToProps = (state) => ({
-    profile: state.profile.profile
+    profile: state.profile.profile,
+    user: state.auth.user
 })
  
 export default connect(mapStateToProps, {getProfile})(Followers);
--- client/src/components/profile/Following.js
+++ client/src/components/profile/Following.js
@@ -2,9 +2,9 @@ import {Button} from 'reactstrap';
 import { getProfile } from '../../actions/profileActions';
 import { connect } from 'react-redux';
 import { useEffect, useState } from 'react';
-import {useParams} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 
-const Following = ({profile, getProfile}) => {
+const Following = ({profile, getProfile, user}) => {
 
     const { id } = useParams();
     const [Loading, setLoading] = useState(true);
@@ -19,28 +19,24 @@ const Following = ({profile, getProfile}) => {
             <div className="height-100 row d-flex justify-content-center">
                 <div className="col-md-7">
                 <h3 className="mb-2">Following</h3>
+                {profile && profile.following.map((following) => (
                     <div className="p-3 bg-white rounded">
                         <div className="d-flex justify-content-between">
                             <div className="d-flex flex-row align-items-center">
+                            <Link to={`/profile/${following.followingId}`}>
                                 <div className="position-relative">
-                                    <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" width="80" className="rounded-circle"/>
+                                    <img src={following.followingPic} width="40" className="rounded-circle"/>
                                 </div>
+                            </Link>
                                 <div className="ml-2">
-                                    <h5 className="mb-0">Kumar Shubham</h5>
-                                    <div className="d-flex flex-row">
-                                        <span className="mr-3">725 followers</span> 
-                                        <span>120 following</span>
-                                    </div>
-                                    <div class="mt-2">
-                                        <Button color="info" className="btn-sm">Follow</Button>
-                                        <Button color="danger" className="btn-sm">Unfollow</Button>
-                                    </div>
+                                    <Link to={`/profile/${following.followingId}`}>
+                                        <h5 className="mb-0">{following.followingName}</h5>
+                                    </Link>
                                 </div>
                             </div>
-                            
-                            <div> </div>
                         </div>
                     </div>
+                ))}
                 </div>
             </div>
         </div>
@@ -48,7 +44,8 @@ const Following = ({profile, getProfile}) => {
 }
 
 const mapStateToProps = (state) => ({
-    profile: state.profile.profile
+    profile: state.profile.profile,
+    user: state.auth.user
 })
 
 export default connect(mapStateToProps, {getProfile})(Following);
