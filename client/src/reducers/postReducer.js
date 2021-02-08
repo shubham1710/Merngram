b0a8c8e130a7fb4ce41eba9b21181ad1db558248
shubham1710 <shubhamstudent5@gmail.com>
2021-02-08 08:59:35 +0530

    Added post reducer

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,62 @@
+import { POST_LOADING, GET_ALL_POSTS, GET_SINGLE_POST, GET_USER_POSTS, GET_FOLLOWING_POSTS, ADD_POST, DELETE_POST } from '../actions/types';
+
+const initialState = {
+    allPosts: [],
+    followingPosts: [],
+    userPosts: [],
+    singlePost: null,
+    loading: false
+}
+
+export default function(state=initialState, action){
+    switch(action.type){
+        case GET_ALL_POSTS:
+            return{
+                ...state,
+                allPosts: action.payload,
+                loading: false
+            }
+        
+        case GET_SINGLE_POST:
+            return{
+                ...state,
+                singlePost: action.payload,
+                loading: false
+            }
+        
+        case GET_USER_POSTS:
+            return{
+                ...state,
+                userPosts: action.payload,
+                loading: false
+            }
+
+        case GET_FOLLOWING_POSTS:
+            return{
+                ...state,
+                followingPosts: action.payload,
+                loading: false
+            }
+
+        case ADD_POST:
+            return{
+                ...state,
+                allPosts: [action.payload, ...state.allPosts]
+            }
+
+        case DELETE_POST:
+            return{
+                ...state,
+                allPosts: state.allPosts.filter(post => post._id!==action.payload)
+            }
+
+        case POST_LOADING:
+            return{
+                ...state,
+                loading: true
+            }
+
+        default:
+            return state;
+    }
+}
