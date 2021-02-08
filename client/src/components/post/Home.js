41d68d9684b46cae782ed57df00ceed1e8dda7ed
shubham1710 <shubhamstudent5@gmail.com>
2021-02-08 10:20:35 +0530

    changed ui a little bit

--- client/src/components/post/Home.js
+++ client/src/components/post/Home.js
@@ -1,37 +1,56 @@
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
-import {Card, Button, CardBody, CardImg, CardText} from 'reactstrap';
+import {Card, Button, CardBody, CardImg, CardText, CardHeader} from 'reactstrap';
 
 class Home extends Component {
     render(){
         return(
-            <div class="container">
+            <div className="container">
                 <div className="row">
-                    <div className="col-md-8 mx-auto mb-3">
-                        <Card>
+                    <div className="col-md-8 mx-auto">
+                        <Card className="mb-4 card-signin">
+                            <CardHeader>
+                                <img src="https://firebasestorage.googleapis.com/v0/b/merngram-2f709.appspot.com/o/user.png?alt=media&token=469f5059-dc59-4a59-8f42-75cf19b0aec8" 
+                                    class="rounded-circle"
+                                    alt="User"
+                                    width="30px"
+                                />
+                                <span className="ml-2"><b>Shubham</b></span><br/>
+                                <small>Posted on 5th February</small>
+                            </CardHeader>
                             <CardBody>
                                 <Link to={`/post/id`}>
                                     <CardImg src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" className="img-fluid rounded shadow-sm"></CardImg>
                                 </Link>
-                                <CardText tag="h5" className="mt-2">Beautiful picture</CardText>
+                                <CardText tag="h6" className="mt-3">Beautiful picture</CardText>
+                                <hr my-1/>
                                 <Button color="success" className="mr-2">Like</Button>
                                 <Button color="danger" className="mr-2">Unlike</Button>
                                 <Link to={`/post/id`}><Button color="info">Comment</Button></Link>
                             </CardBody>
                         </Card>
-                    </div>
-                    <div className="col-md-8 mx-auto mb-3">
-                        <Card>
+                        <Card className="mb-4 card-signin">
+                            <CardHeader>
+                                <img src="https://firebasestorage.googleapis.com/v0/b/merngram-2f709.appspot.com/o/user.png?alt=media&token=469f5059-dc59-4a59-8f42-75cf19b0aec8" 
+                                    class="rounded-circle"
+                                    alt="User"
+                                    width="30px"
+                                />
+                                <span className="ml-2"><b>Satyam</b></span><br/>
+                                <small>Posted on 5th February</small>
+                            </CardHeader>
                             <CardBody>
                                 <Link to={`/post/id`}>
                                     <CardImg src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" className="img-fluid rounded shadow-sm"></CardImg>
                                 </Link>
-                                <CardText tag="h5" className="mt-2">Beautiful picture</CardText>
+                                <CardText tag="h6" className="mt-3">Beautiful picture</CardText>
+                                <hr my-1/>
                                 <Button color="success" className="mr-2">Like</Button>
                                 <Button color="danger" className="mr-2">Unlike</Button>
                                 <Link to={`/post/id`}><Button color="info">Comment</Button></Link>
                             </CardBody>
                         </Card>
+                        
                     </div>
                 </div>
             </div>
--- client/src/components/post/PostDetail.js
+++ client/src/components/post/PostDetail.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import {Row, Card, Button, Form, FormGroup, Label, Input, CardBody, CardText, CardImg} from 'reactstrap';
+import {Row, Card, Button, Form, FormGroup, Label, Input, CardBody, CardText, CardImg, CardHeader} from 'reactstrap';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { set } from 'mongoose';
@@ -15,10 +15,19 @@ const PostDetail = () => {
     }
 
     return ( 
-        <div class="container">
+        <div className="container">
             <div className="row">
                 <div className="col-md-8 mx-auto mb-3">
-                    <Card>
+                    <Card className="card-signin">
+                        <CardHeader>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/merngram-2f709.appspot.com/o/user.png?alt=media&token=469f5059-dc59-4a59-8f42-75cf19b0aec8" 
+                                class="rounded-circle"
+                                alt="User"
+                                width="30px"
+                            />
+                            <span className="ml-2"><b>Shubham</b></span><br/>
+                            <small>Posted on 5th February</small>
+                        </CardHeader>
                         <CardBody>
                             <CardImg src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" className="img-fluid rounded shadow-sm"></CardImg>
                             <CardText tag="h5" className="mt-2">Beautiful picture</CardText>
@@ -36,28 +45,28 @@ const PostDetail = () => {
                         <Button color="primary" className="btn btn-lg btn-block text-uppercase" type="submit">Comment</Button>
                     </Form>
                     
-                    <div class="container d-flex justify-content-center mt-50 mb-50">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <Card>
-                                    <CardBody>
+                    <div className="container d-flex justify-content-center mt-50 mb-50">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <Card >
+                                    <CardHeader>
                                         <CardText tag="h4">Comments</CardText>
-                                    </CardBody>
-                                    <motion.div class="comment-widgets m-b-20" layout>
-                                        <motion.div class="d-flex flex-row comment-row" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }}>
-                                            <div class="p-2"><span class="round"><img src="https://firebasestorage.googleapis.com/v0/b/merngram-2f709.appspot.com/o/user.png?alt=media&token=469f5059-dc59-4a59-8f42-75cf19b0aec8" alt="user" width="50"/></span></div>
-                                            <div class="comment-text w-100">
+                                    </CardHeader>
+                                    <motion.div className="comment-widgets m-b-20" layout>
+                                        <motion.div className="d-flex flex-row comment-row" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }}>
+                                            <div className="p-2"><span className="round"><img src="https://firebasestorage.googleapis.com/v0/b/merngram-2f709.appspot.com/o/user.png?alt=media&token=469f5059-dc59-4a59-8f42-75cf19b0aec8" alt="user" width="30"/></span></div>
+                                            <div className="comment-text w-100">
                                                 <h5>Shubham</h5>
-                                                <div class="comment-footer"> <span class="date">April 14, 2019</span></div>
-                                                <p class="m-b-5 m-t-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it</p>
+                                                <div className="comment-footer"> <span className="date">April 14, 2019</span></div>
+                                                <p className="m-b-5 m-t-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it</p>
                                             </div>
                                         </motion.div>
-                                        <motion.div class="d-flex flex-row comment-row" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }}>
-                                            <div class="p-2"><span class="round"><img src="https://firebasestorage.googleapis.com/v0/b/merngram-2f709.appspot.com/o/user.png?alt=media&token=469f5059-dc59-4a59-8f42-75cf19b0aec8" alt="user" width="50"/></span></div>
-                                            <div class="comment-text active w-100">
+                                        <motion.div className="d-flex flex-row comment-row" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }}>
+                                            <div className="p-2"><span className="round"><img src="https://firebasestorage.googleapis.com/v0/b/merngram-2f709.appspot.com/o/user.png?alt=media&token=469f5059-dc59-4a59-8f42-75cf19b0aec8" alt="user" width="30"/></span></div>
+                                            <div className="comment-text active w-100">
                                                 <h5>Satyam</h5>
-                                                <div class="comment-footer"> <span class="date">March 13, 2020</span> </div>
-                                                <p class="m-b-5 m-t-10">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites</p>
+                                                <div className="comment-footer"> <span className="date">March 13, 2020</span> </div>
+                                                <p className="m-b-5 m-t-10">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites</p>
                                             </div>
                                         </motion.div>                                        
                                     </motion.div>
