a995a4b398e2187b553782cd14ba09a73b5eb92c
shubham1710 <shubhamstudent5@gmail.com>
2021-02-08 18:36:51 +0530

    Added explore section

--- client/src/components/post/Explore.js
+++ client/src/components/post/Explore.js
@@ -1,40 +1,37 @@
 import { motion } from 'framer-motion';
+import PropTypes from 'prop-types';
+import { getAllPosts } from '../../actions/postActions';
+import { connect } from 'react-redux';
+import { useEffect, useState } from 'react';
 
-const Explore = () => {
+const Explore = ({post, getAllPosts}) => {
+
+    const [isPending, setIsPending] = useState(true);
+
+    useEffect(() => {
+        getAllPosts();
+        setIsPending(false);
+    },[isPending])
+
     return ( 
         <div className="container">
             <div className="row">
-                <div className="col-md-3 mb-3">
-                    <motion.img src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" className="img-fluid rounded shadow-sm"
-                     initial={{ opacity: 0 }}
-                     animate={{ opacity: 1 }}
-                     transition={{ delay: 0.5 }}
-                    />
-                </div>
-                <div className="col-md-3 mb-3">
-                    <motion.img src="https://images.unsplash.com/photo-1493571716545-b559a19edd14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" className="img-fluid rounded shadow-sm"
-                     initial={{ opacity: 0 }}
-                     animate={{ opacity: 1 }}
-                     transition={{ delay: 0.5 }}
-                    />
-                </div>
-                <div className="col-md-3 mb-3">
-                    <motion.img src="https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="" className="img-fluid rounded shadow-sm"
-                     initial={{ opacity: 0 }}
-                     animate={{ opacity: 1 }}
-                     transition={{ delay: 0.5 }}
-                    />
-                </div>
-                <div className="col-md-3 mb-3">
-                    <motion.img src="https://images.unsplash.com/photo-1475724017904-b712052c192a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" className="img-fluid rounded shadow-sm"
-                     initial={{ opacity: 0 }}
-                     animate={{ opacity: 1 }}
-                     transition={{ delay: 0.5 }}
-                    />
-                </div>
+                {post.allPosts && post.allPosts.map((post)=>(
+                    <div className="col-md-3 mb-3">
+                        <motion.img src={post.image} alt="" className="img-fluid rounded shadow-sm"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 0.5 }}
+                        />
+                    </div>
+                ))}
             </div>
         </div>
     );
 }
+
+const mapStateToProps = (state) => ({
+    post: state.post
+})
  
-export default Explore;
+export default connect(mapStateToProps, {getAllPosts})(Explore);
