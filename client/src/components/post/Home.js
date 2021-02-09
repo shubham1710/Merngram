96a0369141ca354ac255d77195e1089a86ab48b4
shubham1710 <shubhamstudent5@gmail.com>
2021-02-09 19:29:19 +0530

    Posts loading and display on homepage done

--- client/src/components/post/Home.js
+++ client/src/components/post/Home.js
@@ -1,56 +1,71 @@
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import {Card, Button, CardBody, CardImg, CardText, CardHeader} from 'reactstrap';
+import { getFollowingPosts } from '../../actions/postActions';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Home extends Component {
+
+    state = {
+        postLoaded: false,
+    }
+
+    static propTypes = {
+        getFollowingPosts: PropTypes.func.isRequired,
+        isAuthenticated: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        post: PropTypes.object.isRequired,
+    }
+
+    ongetFollowingPosts = async (userId) => {
+        await this.props.getFollowingPosts(userId);
+        this.setState({postLoaded: true});
+    }
+
     render(){
+        const user = this.props.user;
+        const posts = this.props.post.followingPosts;
+
+        if(this.props.isAuthenticated && !this.props.post.loading && !this.state.postLoaded){
+            this.ongetFollowingPosts(user._id);
+        }
+
         return(
             <div className="container">
                 <div className="row">
                     <div className="col-md-8 mx-auto">
-                        <Card className="mb-4 card-signin">
+                        {!user &&
+                            <Card className="card-signin my-5">
+                                <CardBody>
+                                    <h5 className="card-title text-center"><b>Login to view this page!</b></h5>
+                                    <div className="form-signin">
+                                        <Link to="/login"><Button color="success" className="text-uppercase btn-block mb-2">Login</Button></Link>
+                                        <Link to="/explore"><Button color="info" className="text-uppercase btn-block">See All Photos</Button></Link>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        }
+                        {user && posts && posts.map((post)=>(<Card className="mb-4 card-signin">
                             <CardHeader>
-                                <img src="https://firebasestorage.googleapis.com/v0/b/merngram-2f709.appspot.com/o/user.png?alt=media&token=469f5059-dc59-4a59-8f42-75cf19b0aec8" 
+                                <img src={post.pic} 
                                     class="rounded-circle"
                                     alt="User"
                                     width="30px"
                                 />
-                                <span className="ml-2"><b>Shubham</b></span><br/>
-                                <small>Posted on 5th February</small>
+                                <span className="ml-2"><b>{post.name}</b></span><br/>
                             </CardHeader>
                             <CardBody>
-                                <Link to={`/post/id`}>
-                                    <CardImg src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" className="img-fluid rounded shadow-sm"></CardImg>
+                                <Link to={`/post/${post._id}`}>
+                                    <CardImg src={post.image}></CardImg>
                                 </Link>
-                                <CardText tag="h6" className="mt-3">Beautiful picture</CardText>
+                                <CardText tag="h6" className="mt-3">{post.desc}</CardText>
                                 <hr my-1/>
                                 <Button color="success" className="mr-2">Like</Button>
                                 <Button color="danger" className="mr-2">Unlike</Button>
-                                <Link to={`/post/id`}><Button color="info">Comment</Button></Link>
+                                <Link to={`/post/${post._id}`}><Button color="info">Comment</Button></Link>
                             </CardBody>
-                        </Card>
-                        <Card className="mb-4 card-signin">
-                            <CardHeader>
-                                <img src="https://firebasestorage.googleapis.com/v0/b/merngram-2f709.appspot.com/o/user.png?alt=media&token=469f5059-dc59-4a59-8f42-75cf19b0aec8" 
-                                    class="rounded-circle"
-                                    alt="User"
-                                    width="30px"
-                                />
-                                <span className="ml-2"><b>Satyam</b></span><br/>
-                                <small>Posted on 5th February</small>
-                            </CardHeader>
-                            <CardBody>
-                                <Link to={`/post/id`}>
-                                    <CardImg src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" className="img-fluid rounded shadow-sm"></CardImg>
-                                </Link>
-                                <CardText tag="h6" className="mt-3">Beautiful picture</CardText>
-                                <hr my-1/>
-                                <Button color="success" className="mr-2">Like</Button>
-                                <Button color="danger" className="mr-2">Unlike</Button>
-                                <Link to={`/post/id`}><Button color="info">Comment</Button></Link>
-                            </CardBody>
-                        </Card>
-                        
+                        </Card>))}
                     </div>
                 </div>
             </div>
@@ -58,4 +73,10 @@ class Home extends Component {
     }
 }
 
-export default Home;
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    user: state.auth.user,
+    post: state.post
+})
+
+export default connect(mapStateToProps,{getFollowingPosts})(Home);
