fe5c07f70c66e0fc7ecac75acc6f9e7d4e95961f
shubham1710 <shubhamstudent5@gmail.com>
2021-02-20 08:22:39 +0530

    Changed minor bits

--- client/src/components/post/Home.js
+++ client/src/components/post/Home.js
@@ -27,7 +27,7 @@ class Home extends Component {
         const user = this.props.user;
         const posts = this.props.post.allPosts;
 
-        if(this.props.isAuthenticated && !this.props.post.loading && !this.state.postLoaded){
+        if(!this.props.post.loading && !this.state.postLoaded){
             this.ongetPosts();
         }
 
@@ -35,29 +35,7 @@ class Home extends Component {
             <div className="container">
                 <div className="row">
                     <div className="col-md-8 mx-auto">
-                        {!user &&
-                            <Card className="card-signin my-5">
-                                <CardBody>
-                                    <h5 className="card-title text-center"><b>Login to view this page!</b></h5>
-                                    <div className="form-signin">
-                                        <Link to="/login"><Button color="success" className="text-uppercase btn-block mb-2">Login</Button></Link>
-                                        <Link to="/explore"><Button color="info" className="text-uppercase btn-block">See All Photos</Button></Link>
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        }
-                        {user && posts.length==0 &&
-                            <Card className="card-signin my-5">
-                                <CardBody>
-                                    <h5 className="card-title text-center"><b>Please post something to view in feed!</b></h5>
-                                    <div className="form-signin">
-                                        <Link to="/new"><Button color="primary" className="text-uppercase btn-block mb-2">Add Photo</Button></Link>
-                                        <Link to="/explore"><Button color="info" className="text-uppercase btn-block">See All Photos</Button></Link>
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        }
-                        {user && posts && posts.map((post)=>(<Card className="mb-4 card-signin">
+                        {posts && posts.map((post)=>(<Card className="mb-4 card-signin">
                             <CardHeader>
                                 <img src={post.pic} 
                                     class="rounded-circle"
@@ -65,9 +43,10 @@ class Home extends Component {
                                     alt="User"
                                     width="30px"
                                 />
-                                {post.userId !== user._id ? 
-                                <a href={`/profile/${post.userId}`}><span className="ml-2"><b>{post.name}</b></span><br/></a>: 
-                                <Link to={`/profile/${post.userId}`}><span className="ml-2"><b>{post.name}</b></span><br/></Link>}
+                                { user && post.userId === user._id ?
+                                <Link to={`/profile/${post.userId}`}><span className="ml-2"><b>{post.name}</b></span><br/></Link>:
+                                <a href={`/profile/${post.userId}`}><span className="ml-2"><b>{post.name}</b></span><br/></a> 
+                                }
                             </CardHeader>
                             <CardBody>
                                 <a href={`/post/${post._id}`}>
